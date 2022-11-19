baac8fedabbde30a9ab1e87b03646bfb20dbbd2a
Lucky <luckysikolia7@gmail.com>
2022-11-20 00:22:31 +0300

    Added the calender view

--- app/src/main/java/com/example/wental3/MainActivity.kt
+++ app/src/main/java/com/example/wental3/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.wental3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wental3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //initialize the views
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)//set the content view as the binding view
+
+        //initializing the binding class
+
+        
+//        binding.calView.setOnDateChangeListener { calendarView, i, i2, i3 ->  }
+
+//        val calenderView = binding.root
+//        setContentView(calenderView)
+//
+//        //referencing the views in activity_main
+//        binding.calView.setOnDateChangeListener{ viewModel.userClicked()}
+
+
     }
 }
