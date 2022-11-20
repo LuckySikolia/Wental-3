0f38be89aaa8687f19d16a7c526339ba8ef5f410
Lucky <luckysikolia7@gmail.com>
2022-11-21 01:09:06 +0300

    Fix: interactive navigation bar, view binding

--- app/src/main/java/com/example/wental3/MainActivity.kt
+++ app/src/main/java/com/example/wental3/MainActivity.kt
@@ -2,27 +2,46 @@ package com.example.wental3
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.wental3.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     //initialize the views
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)//set the content view as the binding view
+        //when app is launched for the first time show the home fragment
+        replaceFragment(CalenderFragment())
 
-        //initializing the binding class
+        //implementing on click events for the nav bar
+        binding.bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.calender ->replaceFragment(CalenderFragment())
+                R.id.journal -> replaceFragment(JournalFragment())
+                R.id.stats -> replaceFragment(StatsFragment())
+                R.id.settings -> replaceFragment(SettingsFragment())
 
-        
-//        binding.calView.setOnDateChangeListener { calendarView, i, i2, i3 ->  }
+                else -> {
 
-//        val calenderView = binding.root
-//        setContentView(calenderView)
-//
-//        //referencing the views in activity_main
-//        binding.calView.setOnDateChangeListener{ viewModel.userClicked()}
+                }
 
+            }
+            true
+        }
 
+    }
+
+    //method to replace frame layout in activity_main_xml with the fragments
+    private fun replaceFragment(fragment: Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.calViewFragment, fragment)
+        fragmentTransaction.commit()
     }
 }
