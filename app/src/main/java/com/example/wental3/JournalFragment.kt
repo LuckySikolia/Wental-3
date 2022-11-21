d5b78245a711352b49775574a793047b82bea387
Lucky <luckysikolia7@gmail.com>
2022-11-21 17:11:43 +0300

    fix: view binding problem    : Add floating action bar

--- app/src/main/java/com/example/wental3/JournalFragment.kt
+++ app/src/main/java/com/example/wental3/JournalFragment.kt
@@ -1,21 +1,34 @@
 package com.example.wental3
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.wental3.databinding.FragmentJournalBinding
 
 
 class JournalFragment : Fragment() {
+
+    private lateinit var binding: FragmentJournalBinding
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        binding = FragmentJournalBinding.inflate(inflater,container,false)
+        return binding.root
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_journal, container, false)
+//        return inflater.inflate(R.layout.fragment_journal, container, false)
     }
+
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        binding.apply {
+//            recyclerViewJournal
+//        }
+//    }
 
 }
--- app/src/main/java/com/example/wental3/MainActivity.kt
+++ app/src/main/java/com/example/wental3/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.example.wental3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.wental3.databinding.ActivityMainBinding
 
@@ -10,18 +11,22 @@ class MainActivity : AppCompatActivity() {
     //initialize the views
     private lateinit var binding: ActivityMainBinding
 
+    //for fab (floating action button)
+//    lateinit var fab: FloatingActionButton
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)//set the content view as the binding view
+        setContentView(binding.root)       //set the content view as the binding view\\
+
         //when app is launched for the first time show the home fragment
         replaceFragment(CalenderFragment())
 
         //implementing on click events for the nav bar
         binding.bottomNav.setOnItemSelectedListener {
-            when(it.itemId){
-                R.id.calender ->replaceFragment(CalenderFragment())
+            when (it.itemId) {
+                R.id.calender -> replaceFragment(CalenderFragment())
                 R.id.journal -> replaceFragment(JournalFragment())
                 R.id.stats -> replaceFragment(StatsFragment())
                 R.id.settings -> replaceFragment(SettingsFragment())
@@ -33,15 +38,15 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+    }
+
+        private fun replaceFragment(fragment: Fragment){
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.calViewFragment, fragment)
+            fragmentTransaction.commit()
+        }
 
     }
 
-    //method to replace frame layout in activity_main_xml with the fragments
-    private fun replaceFragment(fragment: Fragment){
 
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.calViewFragment, fragment)
-        fragmentTransaction.commit()
-    }
-}
